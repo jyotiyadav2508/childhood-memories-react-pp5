df6f9e64f02edfc75e6bf9bdec80bbca382c2a42
jyotiyadav2508 <>
2023-06-03 07:15:56 +0000

    Create custom hook for NavBar burger toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 
 /**
@@ -15,6 +16,8 @@ import axios from "axios";
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
      /**
    * Handles user logout
@@ -86,14 +89,17 @@ const NavBar = () => {
   );
 
     return (
-      <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="60" />
           </Navbar.Brand></NavLink>
           {currentUser && addPostIcon}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle 
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto text-left">
       <NavLink exact
